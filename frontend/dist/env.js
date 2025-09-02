9ec89454b0de1147e4d1b5af828893e6f4cd85e8
Aiden Johnson <65328952+tgmstudios@users.noreply.github.com>
2025-09-02 14:54:22 +0000

    build fix

---
+++ frontend/dist/env.js
@@ -0,0 +1,9 @@
+window.ENV = {
+  VITE_API_BASE_URL: 'http://localhost:3011/api',
+  VITE_AUTHENTIK_BASE_URL: 'https://auth.psuccso.org',
+  VITE_AUTHENTIK_CLIENT_ID: 'pKDXlwZKcwt8fWxdZcu1DW4JMqVvPHUiTa5a1RG4',
+  VITE_AUTHENTIK_REDIRECT_URI: 'http://localhost:3010/auth/callback',
+  
+  VITE_DISCORD_CLIENT_ID: '1376436278538539148',
+  VITE_DISCORD_REDIRECT_URI: 'http://localhost:3010/join/dd-verify'
+  }; 
