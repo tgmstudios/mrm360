5436068d4eb2f6fe085fb3a5b69ea2dd67fd3391
BMO Agent <bmo@tgmstudios.net>
2026-02-17 08:09:01 -0800

    refactor: remove team visibility info message from events list
    Removed redundant 'Events linked to a team are only visible to that team's members' message from EventsList filters. This information is now in the EventEdit 'Need Help?' section.

--- frontend/src/pages/events/EventsList.vue
+++ frontend/src/pages/events/EventsList.vue
@@ -124,12 +124,6 @@
               {{ team.name }}
             </option>
           </select>
-          <p class="text-xs text-amber-400/80 mt-1">
-            <svg class="w-3 h-3 inline mr-1" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
-            </svg>
-            Events linked to a team are only visible to that team's members
-          </p>
         </div>
         
         <!-- Status -->
